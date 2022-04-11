023225014e6fbfb94b5638c39e687a6cf800b584
“Davoud <davoud.rk@gmail.com>
2022-04-10 21:28:53 -0400

    init-3

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
 // index.js
 
 const { auth } = require("express-openid-connect");
+const express = require("express");
+const app = express();
+// const port = app.env.PORT || 8000;
+const port = 000;
+require("dotenv").config();
+
 app.use(
   auth({
-    issuerBaseURL: "https://YOUR_DOMAIN",
-    baseURL: "https://YOUR_APPLICATION_ROOT_URL",
-    clientID: "YOUR_CLIENT_ID",
-    secret: "LONG_RANDOM_STRING",
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
     idpLogout: true,
   })
 );
-const express = require("express");
-const app = express();
-require("dotenv").config();
 
-const port = app.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
